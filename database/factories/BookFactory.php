829d3a53f15ab3a1bfecde1b6ccd7ea2e1f3369b
Oniya Daniel <daniel.prince84@gmail.com>
2022-04-08 19:21:07 +0100

    correction to docker-compose, created migration for books

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "name" => $this->faker->company(),
+            "isbn" => $this->faker->isbn13(),
+            "author" => [
+                $this->faker->name()
+            ],
+            "country" => $this->faker->country(),
+            "number_of_pages" => rand(100, 999),
+            "publisher" => $this->faker->name(),
+            "release_date" => $this->faker->dateTime->format('Y-m-d'),
+        ];
+    }
+}
--- database/migrations/2022_04_08_125044_create_books_table.php
+++ database/migrations/2022_04_08_125044_create_books_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("isbn")->unique();
+            $table->string("author");
+            $table->string("country");
+            $table->integer("number_of_pages");
+            $table->string("publisher");
+            $table->dateTime("release_date");
             $table->timestamps();
         });
     }
